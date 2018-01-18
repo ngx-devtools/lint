c8eb99d3e6310abd2c546a75983e31aa223fb770
work <work@override.local>
2018-01-18 23:47:17 +0800

    update the ts linter and version

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+
+const { resolve } = require('path');
+
+if (!(process.env.APP_ROOT_PATH)) {
+  process.env.APP_ROOT_PATH = resolve();
+}
+
 exports.tslint = require('./utils/ts-linter');
 exports.reporter = require('./utils/ts-linter-report');
--- utils/ts-linter.js
+++ utils/ts-linter.js
@@ -1,5 +1,6 @@
 const { Transform } = require('stream');
-const { resolve, join } = require('path');
+const { join } = require('path');
+const { existsSync } = require('fs');
 const { Linter, Configuration } = require('tslint');
 
 const lintOptions = {
@@ -7,21 +8,22 @@ const lintOptions = {
   formattersDirectory: "node_modules/custom-tslint-formatters/formatters",
   formatter: 'grouped',
 };
+const tslintConfigPath = join(process.env.APP_ROOT_PATH, 'tslint.json');
 
-class LinterTransform extends Transform {
+const tslinter = () => { 
+  const transformStream = new Transform({
+    objectMode: true,
+    transform (file, enc, done) {
+      if (existsSync(tslintConfigPath)){
+        const linter = new Linter(lintOptions);
+        const configuration = Configuration.findConfiguration(tslintConfigPath, file.path).results;
+        linter.lint(file.path, file.contents.toString('utf8'), configuration);
+        file.tslint = linter.getResult();
+      }
+      done(null, file);
+    }
+  });
+  return transformStream;
+};
 
-  constructor(){
-    super({ objectMode: true });
-  }
-
-  _transform(file, enc, done) {
-    const linter = new Linter(lintOptions);
-    const configuration = Configuration.findConfiguration(join(resolve(), 'tslint.json'), file.path).results;
-    linter.lint(file.path, file.contents.toString('utf8'), configuration);
-    file.tslint = linter.getResult();
-    done(null, file);
-  }
-  
-}
-
-module.exports = () => new LinterTransform();
+module.exports = tslinter;
